7a0d607cebd999b2313d700d3d86afd71248ec47
jcob95 <jcob95@gmail.com>
2021-10-04 22:18:21 +0200

    Add functionality to operator buttons

--- script.js
+++ script.js
@@ -1,3 +1,77 @@
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const currentScreen = document.getElementById('currentOperationScreen');
+const lastScreen = document.getElementById('lastOperationScreen');
+const equalsButton = document.getElementById('equals');
+const decimalButton = document.getElementById('decimal-point');
+const deleteButton = document.getElementById('delete-btn');
+const acButton = document.getElementById('ac-btn');
+const operatorButtons = document.querySelectorAll('[data-operator]')
+
+//status variables
+let shouldResetScreen = false; // should we clear current screen after entering first values and operations
+let currentOperation = null;
+
+//add event listener functions
+decimalButton.addEventListener('click', () => {appendDecimalPoint()});
+deleteButton.addEventListener('click', () => {deleteNumber()});
+acButton.addEventListener('click',()=>{clearAll()});
+numberButtons.forEach(button => {
+    button.addEventListener('click', () =>{
+        appendNumber(button.textContent)
+    })
+});
+operatorButtons.forEach(button => {
+    button.addEventListener('click',() =>{
+        //currentOperation = button.textContent;
+        //console.log(`current operator ${button.textContent}`)
+        setOperator(button.textContent);
+    })
+})
+
+function setOperator(operator){
+    if(currentOperation !== null) operate()
+    currentOperation = operator;
+    currentOperand = currentScreen.textContent
+    lastScreen.textContent = `${currentOperand}${currentOperation}`
+    shouldResetScreen = true;
+}
+
+//button functions
+function clearAll(){
+    currentScreen.textContent = '0';
+    lastScreen.textContent = '';
+    currentOperation = null;
+    shouldResetScreen = false;
+}
+
+function appendNumber(value){
+    
+    if(currentScreen.textContent === '0' || shouldResetScreen){
+        console.log("reset screen");
+        resetScreen();
+    }
+    currentScreen.textContent += value;
+    
+}
+
+function resetScreen(){
+    currentScreen.textContent = '';
+    shouldResetScreen = false;
+}
+
+function appendDecimalPoint(){
+    if (currentScreen.textContent.includes('.')){
+        return;
+    } 
+    currentScreen.textContent += '.';
+
+}
+
+function deleteNumber(){
+    currentScreen.textContent = currentScreen.textContent.slice(0,-1);
+}
+
 //functions for operations
 function add(a,b){
     return a + b;
